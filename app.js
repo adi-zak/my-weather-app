b61714d2b05936b3b01d07218c657b614968a634
adi-zak <adizaken@yahoo.com>
2025-02-25 20:16:13 +0200

    added axios

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ function clickSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  cityElement.innerHTML =
+    searchInput.value.charAt(0).toUpperCase() +
+    searchInput.value.slice(1).toLowerCase();
 }
 
 let searchFormElement = document.querySelector("#search-form");
