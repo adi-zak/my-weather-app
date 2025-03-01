78c5a2b449b13482e791a27de834f1bbd890182c
adi-zak <adizaken@yahoo.com>
2025-03-01 14:24:51 +0200

    added search fanctionallity using api

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = `79bdadb7bc363044bcfo6ecd1f34t840`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function clickSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML =
-    searchInput.value.charAt(0).toUpperCase() +
-    searchInput.value.slice(1).toLowerCase();
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", clickSearch);
+
+searchCity("Tel-Aviv");
